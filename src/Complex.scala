5ca271434626b8a7e781d61f82e58b4ba72ae42c
nicolashoury <nicolas.houry@live.fr>
2021-12-19 15:55:17 +0100

    Essai

--- src/Complex.scala
+++ src/Complex.scala
@@ -1,5 +1,8 @@
-class Complex (val a:Double, val b : Double){
+import scala.math.sqrt
+
+class Complex(val a: Double, val b: Double) {
   def +(that: Complex) = new Complex(this.a + that.a, this.b + that.b)
-  def *(that: Complex) = new Complex(this.a * that.a - this.b * that.b, this.a * that.b + this.b * that.a)
-  def module = this*a * this.*a - this.b
+  def *(that: Complex) = new Complex( this.a * that.a - this.b * that.b, this.a * that.b + this.b * that.a)
+  def module: Double = sqrt(this.a * this.a + this.b * this.b)
+
 }
