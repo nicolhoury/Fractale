7790e276f73aada44edb07c7aee96cda4d603cce
nicolashoury <nicolas.houry@live.fr>
2021-12-19 15:49:10 +0100

    Essai

--- src/Complex.scala
+++ src/Complex.scala
@@ -1,4 +1,5 @@
-class Complex (val a:double, val b : double){
+class Complex (val a:Double, val b : Double){
   def +(that: Complex) = new Complex(this.a + that.a, this.b + that.b)
   def *(that: Complex) = new Complex(this.a * that.a - this.b * that.b, this.a * that.b + this.b * that.a)
+  def module = this*a * this.*a - this.b
 }
